adf0135a1eed29a9c3b60112c00867bbc395e1da
Simranaujla <simaujla04@gmail.com>
2026-06-10 01:13:47 -0400

    Implemented profile information retrieval
    Added database integration to retrieve and display authenticated user profile information, including name, email address, and phone number.

--- pages/profile.php
+++ pages/profile.php
@@ -1,3 +1,37 @@
+<?php
+    session_start();
+    require_once '../config/database.php';
+
+    //Verify user login session
+    if (!isset ($_SESSION['user_id'])){
+        header("Location:login.php");
+        exit();
+    }
+
+    //Fetch employee profile information
+
+    $sql = "SELECT full_name,email,phone
+    FROM users 
+    WHERE use_id = ?";
+
+    $stmt = mysqli_prepare($conn,$sql);
+
+    mysqli_stmt_bind_param(
+        $stmt,
+        "i",
+        $_SESSION['user_id']
+
+    );
+
+    mysqli_stmt_execute($stmt);
+
+    $result = mysqli_stmt_get_result($stmt);
+
+    $user = mysqli_fetch_assoc($result);
+
+    ?>
+
+
 <!DOCTYPE html >
 <html>
     <head>
@@ -6,9 +40,9 @@
 
     <body>
         <h1>My Profile</h1>
-        <p><strong>Name:</strong></p>
-        <p><strong>Email:</strong></p>
-        <p><strong>Phone:</strong></p>
+        <p><strong>Name:</strong><?php echo $user['full_name']; ?></p>
+        <p><strong>Email:</strong><?php echo $user['email']; ?></p>
+        <p><strong>Phone:</strong><?php echo $user['phone']; ?></p>
         <br>
 
         <a href = "dashboard.php">Back to Dashboard </a>
