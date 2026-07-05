85006bef9f1aa8acef63536ee919fabf2868e2d4
Simranaujla <simaujla04@gmail.com>
2026-07-05 08:22:37 -0400

    Added employee management page
    Implemented an employer-only employee management page to display employee profiles, including full name, email, phone number, and account status. Added dashboard navigation and restricted page access to authorized employer accounts.

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -27,6 +27,7 @@
             echo "<h3>Employer Dashboard</h3>";
             echo "<p>Manage employees and announcements.</p>";
             echo '<a href="manage_records.php"> Manage Employee Records</a><br><br>';
+            echo '<a href="manage_employees.php"> Manage Employees </a><br><br>';
             echo '<a href="announcements.php">Announcements</a><br><br>';
           }
 
---
+++ pages/manage_employees.php
@@ -0,0 +1,76 @@
+<?php
+session_start();
+require_once '../config/database.php';
+
+//Restrict access to Employer accounts
+
+if (!isset($_SESSION['user_id'])  || $_SESSION['role_id'] != 1){
+    header("Location :dashboard.php");
+    exit();
+}
+
+
+//Fetch employee profile information
+$sql = "SELECT
+user_id,
+full_name,
+email,
+phone,
+account_status,
+FROM users
+WHERE role_id = 2
+ORDER BY full_name";
+
+$result = mysqli_query($conn,$sql);
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Manage Employees</title>
+    </head>
+
+    <body>
+        <h1>Manage Employees</h1>
+        <table border = "1" cellpadding = "8">
+            <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>Status</th>
+            </tr>
+
+      
+        
+        <?php
+
+        //Display employee profiles
+        while ($row = mysqli_fetch_assoc($result)){
+            ?>
+            <tr>
+                <td><?php echo $row['full_name']; ?></td>
+                <td><?php echo $row['email']; ?></td>
+                <td><?php echo $row['phone']; ?></td>
+                <td><?php echo $row['account_status']; ?></td>
+            </tr>
+
+        <?php
+        }
+
+        ?>
+
+        </table>
+
+        <br>
+        <a href = "dashboard.php"> Back to Dashboard </a>
+
+
+     
+     </body>
+
+
+</html>
